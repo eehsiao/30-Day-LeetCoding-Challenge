e1b2e37080770839528ea18126ed1a90a825d79d
船長 <erichsiao@chuanzhangde-MacBook-Pro.local>
2020-04-21 12:12:08 +0800

    add week1 Best Time to Buy and Sell Stock II

--- week1-bestTimetoBuyandSellStockII_test.go
+++ week1-bestTimetoBuyandSellStockII_test.go
@@ -22,12 +22,44 @@
 
 package main
 
-func maxProfit(prices []int) int {
-	var maxProfit int
-	for i := 1; i < len(prices); i++ {
-		if prices[i] > prices[i-1] {
-			maxProfit += prices[i] - prices[i-1]
-		}
+import "testing"
+
+func Test_maxProfit(t *testing.T) {
+	type args struct {
+		prices []int
 	}
-	return maxProfit
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				prices: []int{7, 1, 5, 3, 6, 4},
+			},
+			want: 7,
+		},
+		{
+			name: "case 2",
+			args: args{
+				prices: []int{1, 2, 3, 4, 5},
+			},
+			want: 4,
+		},
+		{
+			name: "case 3",
+			args: args{
+				prices: []int{7, 6, 4, 3, 1},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfit(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit(%v) = %v, want %v", tt.args.prices, got, tt.want)
+			}
+		})
+	}
 }
