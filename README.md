c195d491ab95cdf63068b8bc7f56df8f3cd465b1
船長 <erichsiao@chuanzhangde-MacBook-Pro.local>
2020-04-21 17:08:26 +0800

    add week2 Diameter of Binary Tree

--- README.md
+++ README.md
@@ -198,3 +198,19 @@ Methods pop, top and getMin operations will always be called on non-empty stacks
    Hide Hint #1  
 Consider each node in the stack having a minimum value. (Credits to @aakarshmadhavan)
 ```
+
+###  [Diameter of Binary Tree](https://github.com/eehsiao/30-Day-LeetCoding-Challenge/blob/master/week2-diameterofBinaryTree.go)
+```
+Diameter of Binary Tree
+Solution
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+Example:
+Given a binary tree
+          1
+         / \
+        2   3
+       / \
+      4   5
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+Note: The length of path between two nodes is represented by the number of edges between them.
+```
---
+++ week2-diameterofBinaryTree.go
@@ -0,0 +1,44 @@
+// Diameter of Binary Tree
+// Solution
+// Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+// Example:
+// Given a binary tree
+//           1
+//          / \
+//         2   3
+//        / \
+//       4   5
+// Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+// Note: The length of path between two nodes is represented by the number of edges between them.
+
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func depth(node *TreeNode, ans *int) int {
+	if node == nil {
+		return 0
+	}
+	L := depth(node.Left, ans)
+	R := depth(node.Right, ans)
+	*ans = max(*ans, L+R+1)
+	return max(L, R) + 1
+}
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	var ans int = 1
+	depth(root, &ans)
+	return ans - 1
+}
