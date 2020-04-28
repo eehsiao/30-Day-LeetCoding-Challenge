6d7a850346aa5dc00071268a8761897bbdba1530
船長 <erichsiao@chuanzhangde-MacBook-Pro.local>
2020-04-29 00:02:58 +0800

    week3-constructBinarySearchTreefromPreorderTraversal

--- README.md
+++ README.md
@@ -390,6 +390,23 @@ Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 ```
 
+###  [Construct Binary Search Tree from Preorder Traversal](https://github.com/eehsiao/30-Day-LeetCoding-Challenge/blob/master/week3-constructBinarySearchTreefromPreorderTraversal.go)
+```
+Return the node node of a binary search tree that matches the given preorder traversal.
+(Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+Example 1:
+Input: [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+     8
+    / \
+   5   10
+  / \    \
+ 1   7    12
+Note:
+1 <= preorder.length <= 100
+The values of preorder are distinct.
+```
+
 ## Week 4: April 22nd–April 28th
 ###  [Subarray Sum Equals K](https://github.com/eehsiao/30-Day-LeetCoding-Challenge/blob/master/week4-subarraySumEqualsK.go) [(Test Case)](https://github.com/eehsiao/30-Day-LeetCoding-Challenge/blob/master/week4-subarraySumEqualsK_test.go)
 ```
---
+++ week3-constructBinarySearchTreefromPreorderTraversal.go
@@ -0,0 +1,71 @@
+// Return the node node of a binary search tree that matches the given preorder traversal.
+// (Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+// Example 1:
+// Input: [8,5,1,7,10,12]
+// Output: [8,5,10,1,7,null,12]
+//      8
+// 	   / \
+//    5   10
+//   / \    \
+//  1 	7    12
+// Note:
+// 1 <= preorder.length <= 100
+// The values of preorder are distinct.
+
+package main
+
+// defin in week2-diameterofBinaryTree.go
+// type TreeNode struct {
+// 	Val   int
+// 	Left  *TreeNode
+// 	Right *TreeNode
+// }
+
+var order []int
+
+func preOrder(node *TreeNode) {
+
+	if node.Left != nil {
+		preOrder(node.Left)
+	}
+	order = append(order, node.Val)
+	if node.Right != nil {
+		preOrder(node.Right)
+	}
+}
+
+func bstFromPreorder(preorder []int) *TreeNode {
+	var bTree *TreeNode
+
+	order = make([]int, 0)
+	for _, v := range preorder {
+		newNode := &TreeNode{
+			Val: v,
+		}
+
+		if bTree == nil {
+			bTree = newNode
+			continue
+		}
+		node := bTree
+		for {
+			if node.Val > v {
+				if node.Left != nil {
+					node = node.Left
+				} else {
+					node.Left = newNode
+					break
+				}
+			} else {
+				if node.Right != nil {
+					node = node.Right
+				} else {
+					node.Right = newNode
+					break
+				}
+			}
+		}
+	}
+
+	return bTree
+}
