4d6a0faa1b9cf7c681a1fe72c47ddf9a67a607bb
船長 <erichsiao@chuanzhangde-MacBook-Pro.local>
2020-04-30 23:35:18 +0800

    add img link

--- week5-checkIfaStringIsaValidSequencefromRoottoLeavesPathinaBinaryTree.go
+++ week5-checkIfaStringIsaValidSequencefromRoottoLeavesPathinaBinaryTree.go
@@ -1,6 +1,7 @@
 // Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree.
 // We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
 // Example 1:
+// https://assets.leetcode.com/uploads/2019/12/18/leetcode_testcase_1.png
 // Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
 // Output: true
 // Explanation:
@@ -9,10 +10,12 @@
 // 0 -> 1 -> 1 -> 0
 // 0 -> 0 -> 0
 // Example 2:
+// https://assets.leetcode.com/uploads/2019/12/18/leetcode_testcase_2.png
 // Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
 // Output: false
 // Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
 // Example 3:
+// https://assets.leetcode.com/uploads/2019/12/18/leetcode_testcase_3.png
 // Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,1]
 // Output: false
 // Explanation: The path 0 -> 1 -> 1 is a sequence, but it is not a valid sequence.
